441438cc684410bf3b1639d8f9000d32f8e66865
myselfsoz <121366571+myselfsoz@users.noreply.github.com>
2023-07-01 09:31:42 +0545

    Update AdsServer.js

--- libs/AdsServer.js
+++ libs/AdsServer.js
@@ -2,12 +2,19 @@ let lib_prefix = 'AdsServer';
 let API_URL = 'https://api.telegram.org/bot'+bot.token+'/getChatMember';
 
 function showAds(chat_id,user_id){
+  Api.getChatMember({
+    chat_id: chat_id,
+    user_id: user.telegramid,
+    on_result: lib_prefix + 'onApiResponse',
+    on_error: lib_prefix + 'onApiError'
+  })
+  /*
   let parameter = {
     url: API_URL + '?chat_id='+chat_id + '&user_id=' + user_id,
     success: lib_prefix + 'onApiResponse',
     error: lib_prefix + 'onApiError'
   }
-  HTTP.get(parameter)
+  HTTP.get(parameter)*/
 }
 
 function onApiResponse(){ Bot.sendMessage(content);
